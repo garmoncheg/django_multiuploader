3390caf3a37f5e14130a1293a1a36941b27d4e05
Iurii Garmash <garmon1@gmail.com>
2011-07-18 18:30:28 +0200

    1.0.2 file structure changes

--- multiuploader/admin.py
+++
@@ -1,9 +0,0 @@
-from models import MultiuploaderImage
-from django.contrib import admin
-
-class MultiuploaderImageAdmin(admin.ModelAdmin):
-    search_fields = ["filename", "key_data"]
-    list_display = ["filename", "image", "key_data"]
-    list_filter = ["filename", "image", "key_data"]
-
-admin.site.register(MultiuploaderImage, MultiuploaderImageAdmin)
--- multiuploader/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^delete/(\d+)/$', 'multiuploader.views.multiuploader_delete'),
-    url(r'^multi/$', 'multiuploader.views.multiuploader', name='multi'),
-    (r'^image/(\d+)/$', 'multiuploader.views.multi_show_uploaded'),
-)
