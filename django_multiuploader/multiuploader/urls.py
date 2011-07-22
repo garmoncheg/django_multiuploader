fc4457ca7c72efd9a78b70a5eb102c6fb04576c3
Iurii Garmash <garmon1@gmail.com>
2011-07-22 20:43:32 +0300

    1.1.0 Fixed delete url

--- django_multiuploader/multiuploader/urls.py
+++ django_multiuploader/multiuploader/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+
+if settings.MULTI_FILE_DELETE_URL:
+    delete_url = settings.MULTI_FILE_DELETE_URL
+else:
+    delete_url = 'multi_delete'
 
 urlpatterns = patterns('',
-    (r'^delete/(\d+)/$', 'multiuploader.views.multiuploader_delete'),
+    (r'^'+delete_url+'/(\d+)/$', 'multiuploader.views.multiuploader_delete'),
     url(r'^multi/$', 'multiuploader.views.multiuploader', name='multi'),
     (r'^image/(\d+)/$', 'multiuploader.views.multi_show_uploaded'),
 )
