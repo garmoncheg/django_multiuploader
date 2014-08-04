0a664f2d959db722e783f6f2bd87b0ddc9936a1e
Jayme <kalkehcoisa@gmail.com>
2014-08-04 07:28:06 -0300

    Fix on the import of django.conf.urls

--- django_multiuploader/multiuploader/urls.py
+++ django_multiuploader/multiuploader/urls.py
@@ -1,19 +1,22 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except:
+    from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-                       url(r'^multiuploader_delete_multiple/$', 'multiuploader.views.multiuploader_delete_multiple',
-                           name='multiuploader_delete_multiple'),
-                       url(r'^multiuploader_delete/(?P<pk>\w+)/$', 'multiuploader.views.multiuploader_delete',
-                           name='multiuploader_delete'),
-                       url(r'^multiuploader/$', 'multiuploader.views.multiuploader', name='multiuploader'),
-                       url(r'^multiuploader_noajax/$', 'multiuploader.views.multiuploader', kwargs={"noajax": True},
-                           name='multiploader_noajax'),
-                       url(r'^multiuploader_file/(?P<pk>\w*)/$', 'multiuploader.views.multi_show_uploaded',
-                           name='multiuploader_file_link'),
-                       
-                       
-                       url(r'^multiuploader_get_files_noajax/$', 'multiuploader.views.multi_get_files', kwargs={"noajax": True},
-                           name='multiuploader_get_files_noajax'),
-                       url(r'^multiuploader_get_files/(?P<fieldname>\w*)/$', 'multiuploader.views.multi_get_files',
-                           name='multi_get_files'),
+urlpatterns = patterns(
+    '',
+   url(r'^multiuploader_delete_multiple/$', 'multiuploader.views.multiuploader_delete_multiple',
+       name='multiuploader_delete_multiple'),
+   url(r'^multiuploader_delete/(?P<pk>\w+)/$', 'multiuploader.views.multiuploader_delete',
+       name='multiuploader_delete'),
+   url(r'^multiuploader/$', 'multiuploader.views.multiuploader', name='multiuploader'),
+   url(r'^multiuploader_noajax/$', 'multiuploader.views.multiuploader', kwargs={"noajax": True},
+       name='multiploader_noajax'),
+   url(r'^multiuploader_file/(?P<pk>\w*)/$', 'multiuploader.views.multi_show_uploaded',
+       name='multiuploader_file_link'),
+
+   url(r'^multiuploader_get_files_noajax/$', 'multiuploader.views.multi_get_files', kwargs={"noajax": True},
+       name='multiuploader_get_files_noajax'),
+   url(r'^multiuploader_get_files/(?P<fieldname>\w*)/$', 'multiuploader.views.multi_get_files',
+       name='multi_get_files'),
 )
